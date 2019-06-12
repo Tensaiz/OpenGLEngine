983da54bc29ddfd6da595adf4d5f7aa76e4b1e2f
Tensaiz <matevy1@gmail.com>
2019-06-12 20:41:27 +0200

    Update for tutorial 16

--- src/renderEngine/MasterRenderer.java
+++ src/renderEngine/MasterRenderer.java
@@ -21,6 +21,10 @@ public class MasterRenderer {
     private static final float NEAR_PLANE = 0.1f;
     private static final float FAR_PLANE = 1000;
 
+    private static final float RED = 0.5f;
+    private static final float GREEN = 0.5f;
+    private static final float BLUE = 0.5f;
+
     private Matrix4f projectionMatrix;
     private StaticShader shader = new StaticShader();
     private EntityRenderer entityRenderer;
@@ -49,12 +53,14 @@ public class MasterRenderer {
     public void render(Light sun, Camera camera) {
         prepare();
         shader.start();
+        shader.loadSkyColour(RED, GREEN, BLUE);
         shader.loadLight(sun);
         shader.loadViewMatrix(camera);
         entityRenderer.render(entities);
         shader.stop();
 
         terrainShader.start();
+        terrainShader.loadSkyColour(RED, GREEN, BLUE);
         terrainShader.loadLight(sun);
         terrainShader.loadViewMatrix(camera);
         terrainRenderer.render(terrains);
@@ -89,7 +95,7 @@ public class MasterRenderer {
     public void prepare() {
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
-        GL11.glClearColor(1, 0, 0, 1);
+        GL11.glClearColor(RED, GREEN, BLUE, 1);
     }
 
     private void createProjectionMatrix() {
--- src/shaders/TerrainShader.java
+++ src/shaders/TerrainShader.java
@@ -3,6 +3,7 @@ package shaders;
 import entities.Camera;
 import entities.Light;
 import org.joml.Matrix4f;
+import org.joml.Vector3f;
 import util.Maths;
 
 public class TerrainShader extends ShaderProgram {
@@ -17,6 +18,8 @@ public class TerrainShader extends ShaderProgram {
     private int location_lightColour;
     private int location_shineDamper;
     private int location_reflectivity;
+    private int location_useFakeLighting;
+    private int location_skyColour;
 
     public TerrainShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -38,6 +41,12 @@ public class TerrainShader extends ShaderProgram {
         location_lightColour = super.getUniformLocation("lightColour");
         location_shineDamper = super.getUniformLocation("shineDamper");
         location_reflectivity = super.getUniformLocation("reflectivity");
+        location_useFakeLighting = super.getUniformLocation("useFakeLighting");
+        location_skyColour = super.getUniformLocation("skyColour");
+    }
+
+    public void loadSkyColour(float r, float g, float b) {
+        super.loadVector(location_skyColour, new Vector3f(r, g, b));
     }
 
     public void loadShineVariables(float damper, float reflectivity) {
